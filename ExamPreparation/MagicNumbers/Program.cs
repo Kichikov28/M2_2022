a5d5f8c88af77a081445cd04945e3767613da933
Kichikov28 <dkichikov4@gmail.com>
2022-04-09 12:20:46 +0300

    exam1

--- ExamPreparation/MagicNumbers/Program.cs
+++ ExamPreparation/MagicNumbers/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MagicNumbers
 {
@@ -9,17 +10,17 @@ namespace MagicNumbers
         static void Main(string[] args)
         {
             List<string> nums = Console.ReadLine().Split(',').ToList();
-            List<string> even = new List<string> { };
-            List<string> odd = new List<string> { };
-            List<string> even15 = new List<string> { };
+            List<string> even = new List<string>();
+            List<string> odd = new List<string> ();
+            List<string> even15 = new List<string>();
 
             for (int i = 0; i < nums.Count; i++)
             {
-                if (nums[nums[i].Length - 1] == "8" || nums[nums[i].Length - 1] == "4")
+                if (nums[nums[i].Length - 1].EndsWith("5") || nums[nums[i].Length - 1].EndsWith("7"))
                 {
                     even.Add(nums[i]);
                 }
-                if (nums[nums[i].Length - 1] == "5" || nums[nums[i].Length - 1] == "7")
+                if (nums[nums[i].Length - 1].EndsWith("4") || nums[nums[i].Length - 1].EndsWith("8"))
                 {
                     odd.Add(nums[i]);
                 }
